ae9c8acc04e1de5098b90d9ddeb810780b81be3b
Alana Rafaela <100479441+Anony0-0@users.noreply.github.com>
2026-02-06 22:15:45 -0300

    Create Cafeteira_main.java

---
+++ src/Cafeteira_main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Cafeteira {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Selecione seu café preferido: \n1-Café puro\n2-Café com leite\n3-Café gelado \n3-Espresso\n4-Mocha\n5-Cappucino\n6-Latte\n7-Frappucino"); pularlinha();
+
+        int selecaoCafe = sc.nextInt();
+
+        //arrumar o call da classe      
+        if (selecaoCafe == 2){
+            Cafes.cafeComleite();
+        }
+
+
+       
+    }
+
+    public static void aguarde() {
+        for (int p = 0; p <=2 ; p++){
+            System.out.print("." +" ");
+        }
+    }
+    public static void pularlinha(){
+        System.out.println();
+    }
+}
