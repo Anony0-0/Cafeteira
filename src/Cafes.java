8394e2ac2e69f7f779fb6c0a7e9dd5e1bea3a35f
Alana Rafaela <100479441+Anony0-0@users.noreply.github.com>
2026-02-08 20:38:41 -0300

    Update Cafes.java

--- src/Cafes.java
+++ src/Cafes.java
@@ -1,15 +1,16 @@
 public class Cafes {
 
     int tamanho;
+    float temp;
 
-
-    public void cafePuro(){
+    public void espresso(){
+        //https://www.youtube.com/watch?v=hfskQB2CZQE
         System.out.print("Preparando seu café");Cafeteira.aguarde(); Cafeteira.pularlinha();
         System.out.print("Moendo grãos de café");   Cafeteira.aguarde(); Cafeteira.pularlinha();
         System.out.print("Fervendo a água");   Cafeteira.aguarde(); Cafeteira.pularlinha();
         System.out.print("Misturando a água quente com o café moído");  Cafeteira.aguarde(); Cafeteira.pularlinha();
         System.out.print("Servindo o café no copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
-        System.out.print("Seu café com leite está pronto");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Seu café está pronto!");  Cafeteira.aguarde(); Cafeteira.pularlinha();
 
     }
 
@@ -20,26 +21,38 @@ public class Cafes {
         System.out.print("Misturando a água quente com o café moído");  Cafeteira.aguarde(); Cafeteira.pularlinha();
         System.out.print("Servindo o café no copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
         System.out.print("Servindo um pouco de leite dentro do copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
-        System.out.print("Seu café com leite está pronto");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Seu café com leite está pronto!");  Cafeteira.aguarde(); Cafeteira.pularlinha();
     }
 
-    public void cafeGelado(){
-
-    }
-    public void espresso(){
-
-    }
     public void mocha(){
+        System.out.print("Preparando seu café mocha");Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Moendo grãos de café");   Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Fervendo a água");   Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Misturando a água quente com o café moído");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Servindo o café no copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Servindo um pouco de leite dentro do copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Seu café com leite está pronto!");  Cafeteira.aguarde(); Cafeteira.pularlinha();
 
     }
     public void capuccino(){
+        System.out.print("Preparando seu café capuccino");Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Moendo grãos de café");   Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Fervendo a água");   Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Misturando a água quente com o café moído");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Servindo o café no copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Servindo um pouco de leite dentro do copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Seu café com leite está pronto!");  Cafeteira.aguarde(); Cafeteira.pularlinha();
 
     }
     public void latte(){
-
+        System.out.print("Preparando seu café latte");Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Moendo grãos de café");   Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Fervendo a água");   Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Misturando a água quente com o café moído");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Servindo o café no copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Servindo um pouco de leite dentro do copo");  Cafeteira.aguarde(); Cafeteira.pularlinha();
+        System.out.print("Seu café com leite está pronto!");  Cafeteira.aguarde(); Cafeteira.pularlinha();
     }
-    public void frappucino(){
 
-    }
 
 }
