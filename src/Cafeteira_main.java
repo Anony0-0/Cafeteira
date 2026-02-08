e5a412304f043b03664e15ee4c2353acb4a2a002
Alana Rafaela <100479441+Anony0-0@users.noreply.github.com>
2026-02-08 20:38:16 -0300

    Update Cafeteira_main.java
    Retirei os cafés gelados e repetidos, e adicionei o else

--- src/Cafeteira_main.java
+++ src/Cafeteira_main.java
@@ -5,38 +5,31 @@ public class Cafeteira {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Selecione seu café preferido: \n1-Café puro\n2-Café com leite\n3-Café gelado \n4-Espresso\n5-Mocha\n6-Cappucino\n7-Latte\n8-Frappucino"); pularlinha();
+        System.out.println("Selecione seu café preferido: \n1-Expresso \n2-Café com leite \n3-Mocha\n4-Cappucino\n5-Latte"); pularlinha();
 
         int selecaoCafe = sc.nextInt();
 
         if (selecaoCafe == 1){
-            Cafes cafePruo = new Cafes();
-            cafePruo.cafePuro();
-
+            Cafes espresso = new Cafes();
+            espresso.espresso();
         } else if (selecaoCafe == 2) {
             Cafes cafeComLeite = new Cafes();
             cafeComLeite.cafeComLeite();
-         } else if (selecaoCafe == 3) {
-            Cafes cafeGelado = new Cafes();
-            cafeGelado.cafeGelado();
-        } else if (selecaoCafe == 4) {
-            Cafes espresso = new Cafes();
-            espresso.espresso();
-        } else if (selecaoCafe == 5) {
+        } else if (selecaoCafe == 3) {
             Cafes mocha= new Cafes();
             mocha.mocha();
-        } else if (selecaoCafe == 6) {
+        } else if (selecaoCafe == 4) {
             Cafes capuccino = new Cafes();
             capuccino.capuccino();
-        } else if (selecaoCafe == 7) {
+        } else if (selecaoCafe == 5) {
             Cafes latte = new Cafes();
             latte.latte();
-        } else if (selecaoCafe == 8) {
-            Cafes frappucino= new Cafes();
-            frappucino.frappucino();
+        } else{ //colocar também que se for uma string, a mesma mensagem aparece
+            System.out.println("Por favor, selecione uma opção válida");
         }
 
     }
+
 
 
     public static void aguarde() {
