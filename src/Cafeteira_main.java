8f6802adafc495429974fdee4cfe2a0a5158ee1d
Alana Rafaela <100479441+Anony0-0@users.noreply.github.com>
2026-02-08 20:02:40 -0300

    Update Cafeteira_main.java

--- src/Cafeteira_main.java
+++ src/Cafeteira_main.java
@@ -5,18 +5,39 @@ public class Cafeteira {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Selecione seu café preferido: \n1-Café puro\n2-Café com leite\n3-Café gelado \n3-Espresso\n4-Mocha\n5-Cappucino\n6-Latte\n7-Frappucino"); pularlinha();
+        System.out.println("Selecione seu café preferido: \n1-Café puro\n2-Café com leite\n3-Café gelado \n4-Espresso\n5-Mocha\n6-Cappucino\n7-Latte\n8-Frappucino"); pularlinha();
 
         int selecaoCafe = sc.nextInt();
 
-        //arrumar o call da classe      
-        if (selecaoCafe == 2){
-            Cafes.cafeComLeite();
+        if (selecaoCafe == 1){
+            Cafes cafePruo = new Cafes();
+            cafePruo.cafePuro();
+
+        } else if (selecaoCafe == 2) {
+            Cafes cafeComLeite = new Cafes();
+            cafeComLeite.cafeComLeite();
+         } else if (selecaoCafe == 3) {
+            Cafes cafeGelado = new Cafes();
+            cafeGelado.cafeGelado();
+        } else if (selecaoCafe == 4) {
+            Cafes espresso = new Cafes();
+            espresso.espresso();
+        } else if (selecaoCafe == 5) {
+            Cafes mocha= new Cafes();
+            mocha.mocha();
+        } else if (selecaoCafe == 6) {
+            Cafes capuccino = new Cafes();
+            capuccino.capuccino();
+        } else if (selecaoCafe == 7) {
+            Cafes latte = new Cafes();
+            latte.latte();
+        } else if (selecaoCafe == 8) {
+            Cafes frappucino= new Cafes();
+            frappucino.frappucino();
         }
 
-
-       
     }
+
 
     public static void aguarde() {
         for (int p = 0; p <=2 ; p++){
