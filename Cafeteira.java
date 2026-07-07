5530473cdfc9bdc945b825f589b3ba0caddf929b
Alana Rafaela <100479441+Anony0-0@users.noreply.github.com>
2026-07-06 22:36:44 -0300

    Update and rename cafe_com_leite.java to Cafeteira.java

---
+++ Cafeteira.java
@@ -0,0 +1,14 @@
+import telas.TelaCafeteira;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseListener;
+import java.util.Scanner;
+
+public class Cafeteira {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        new TelaCafeteira();
+
+    }
+
+}
